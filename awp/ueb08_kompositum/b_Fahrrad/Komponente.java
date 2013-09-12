ce7439c3dc974a5ea5b134479524196a20fe7517
t.j <thomas.jonitz@gmail.com>
2013-09-12 15:26:47 +0200

    cleard directory.

--- awp/ueb08_kompositum/b_Fahrrad/Komponente.java
+++
@@ -1,9 +0,0 @@
-public abstract class Komponente {
-
-  String bezeichnung;
-
-  String getBezeichnung() { return bezeichnung; }
-  void setBezeichnung(String bez) { bezeichnung = bez; }
-
-  public abstract double getPreis();
-}
