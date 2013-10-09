6c84cc74e61cd0dfb5e048d8429f3e6276d844ee
t.j <thomas.jonitz@gmail.com>
2013-10-09 17:20:49 +0200

    changed ti/ackermann.c

--- ti/ackermann.c
+++ ti/ackermann.c
@@ -1,67 +1,108 @@
 /**
- * @file    ackermann.c Definiert eine Ackermann-Funktion
- * @author  t.j.
- * @version 2013-08
+ * @file ackermann.c Implements an ackermann function
+ * @author t.j.
+ * @version 2013-06-26
  *
- * @brief   Uebungsaufgaube Theoretische Informatik.
- *          Die Ackermann-Funktion wird in zwei Schleifen durchgeführt, 
- *          in der die Funktion mit den Werten <tt>a</tt> und <tt>b</tt>, 
- *          bis die <tt>MAX_M</tt> und <tt>MAX_N</tt> erreicht haben. Mit den 
- *          Standarteinstellungen wird der zunehmende Zeitbedarf zur 
- *          Berechnung sichtbar.
- */ 
-#include <stdlib.h>
+ * This program implements an Ackermann function that shows
+ * a fast growth of needed runtime.
+ *
+ * Compile:
+ *      $ gcc -Wall -ggdb -o ackermann.o ackermann.c
+ *
+ * Usage:
+ *      Run this program either with two arguments for 'm' and 'n':
+ *              $ ./ackermann.o <m> <n>
+ *      or without any arguments. 'm' will be 5 and 'n' will
+ *      be 14 by default.
+ *
+ * Note:
+ *      This program is very slow due to the complex
+ *      recursive function. It will take a while to
+ *      compute the results or, it will cause a
+ *      buffer overflow (mainly the global iterator).
+ *
+ *      This file is also located in the 'hsw' repository.
+ *              $ git clone http://code.google.com/p/xladde.hsw 
+ *
+ * For more information about the Ackermann function:
+ *      http://en.wikipedia.org/wiki/Ackermann_function
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Maximale Schleifendurchlaeufe
+// define maximum nuber of loops
 #define MAX_M 4
-#define MAX_N 12
+#define MAX_N 14
 
-// definiere einen neuen (vorzeichenlosen) Typ.
-typedef unsigned long long int ulong64;
-
-// globale Hilfsvariable zur übersichtlicheren Darstellung auf dem Bildschirm. 
-// Variable zählt, wie oft die A-Fkt (da rekursiv) tatsächlich aufgerufen wird.
-ulong64 iteration = 0; 
+// some iterator to count inside loops
+unsigned iteration;
 
 /**
- * @brief   Definition der Ackermann-Funktion.
- * @param m Vorzeichenloser Integer-Wert
- * @param n Vorzeichenloser Integer-Wert
- * @return  Ergebnis der Ackermann-Funktion. Long Integer, da das Ergebnis 
- *          einen sehr großen Wert annehmen kann.
+ * @brief Ackermann function.
+ * @param m First argument.
+ * @param n Second argument.
+ * @return Result of calculation.
  */
-ulong64 ackermann( ulong64 m, ulong64 n )
+unsigned ackermann( unsigned m, unsigned n )
 {
-        ++iteration;
-        if( m == 0 ) 
-        { 
-                return n+1;
-        } else if( n == 0 )
-                { return ackermann( m-1, 1 ); } 
-        else
-                { return ackermann( m-1, ackermann(m, n-1) ); }
-} // end ackermann
+    ++iteration;
+    if( m==0 ) 
+        { return n+1; }
+    else if( n==0 ) 
+        { return ackermann(m-1, 1); }
+    else
+        { return ackermann( m-1, ackermann(m, n-1) ); }
+} // end ackermann fnc
 
 /**
- * @brief  Main-Funktion mit Testroutine.
- * @return 0
+ * @brief Main function.
  */
-int main( void )
+int main( int argc, char *argv[] )
 {
-        ulong64 a, b;
-        system( "clear");
-        printf( "Running Ackermann function.\n");
-        printf( "This may take a while or cause a buffer overflow.\n" );
-        for( a = 0; a < MAX_M; a++ )
+    unsigned a, 
+             b, 
+             m, 
+             n, 
+             result;
+    float    runtime;
+    clock_t  start, 
+             stop;
+        
+    iteration = 0;
+    
+    // preparing values    
+    if( argc == 3 )
+    {
+            m = (unsigned)atoi( argv[1] );
+            n = (unsigned)atoi( argv[2] );
+    } else
+    {
+        m = (unsigned)MAX_M;
+            n = (unsigned)MAX_N;
+    }
+    
+    // some output
+    system( "clear" );
+    printf( "Running Ackermann." );
+    printf( "This may take some time or causes a buffer overflow\n" );
+    
+    // here starts the action    
+    for( a=0; a<m; a++ )
+    {
+        for( b=0; b<n; b++ )
         {
-                for( b = 0; b < MAX_N; b++ )
-                {
-                        printf( "(%lld, %lld)\tResult: %lld\tSteps: %lld\n", 
-                                a, b, ackermann( a, b ), iteration );
-                        iteration = 0; // Zaehler zurücksetzen
-                }
-        }
-        printf( "Finished.\n" );
-        return 0;
-}// end main
+            start   = clock();  // start timer
+            result  = ackermann( a, b );
+            stop    = clock();  // stop timer                    
+            runtime = (float)(stop-start)/(float)(CLOCKS_PER_SEC);
+                        
+            printf( "(%u,%u)\t= %u\ttime: %.2f sec\titerations: %d\n", 
+                    a, b, result, runtime, iteration );
+                        
+            iteration = 0;
+        } // end inner for
+    } // end outer for
+    return EXIT_SUCCESS;
+} // end main fnc
+ 
