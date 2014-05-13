dfe892391fb353651121981bf101676da42bab26
xladde <thomas.jonitz@gmail.com>
2014-05-13 17:23:02 +0200

    startet mobag/LinieFolgen

---
+++ mobag/LinieFolgen/src/linie/LinieFahren.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package linie;
+
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+
+/**
+ * @author tj
+ *
+ */
+public class LinieFahren {
+
+    public LightSensor  sensor;
+    public Motor[]      actors;
+    
+    public LinieFahren(LightSensor sensor, Motor[] actors) {
+        
+    }
+	
+}
---
+++ mobag/LinieFolgen/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+/**
+ * 
+ */
+
+/**
+ * @author tj
+ * @version 2014-05-13
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
