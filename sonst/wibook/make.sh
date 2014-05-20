9509829a034afa597dcdbcddc6723e50ebd04fb9
xladde <thomas.jonitz@gmail.com>
2014-05-20 10:56:58 +0200

    update 	deleted:    mobag/LinieFolgen/src/linie/LinieFahren.java 	deleted:    mobag/LinieFolgen/src/main/Main.java 	new file:   sonst/wibook/impress.tex 	modified:   sonst/wibook/make.sh 	modified:   sonst/wibook/wibook.pdf 	modified:   sonst/wibook/wibook.tex 	modified:   tex/hswarticle.cls 	modified:   tex/literature.bib

--- sonst/wibook/make.sh
+++ sonst/wibook/make.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pdflatex wibook.tex
+pdflatex -halt-on-error -draftmode wibook.tex > compile.log
 bibtex wibook.aux
 #pdflatex wibook.tex
 makeindex wibook.toc wibook.lof wibook.lot
-pdflatex wibook.tex
+pdflatex -halt-on-error -draftmode wibook.tex >> compile.log
+pdflatex -halt-on-error wibook.tex >> compile.log
+
 
 rm *.aux *.lot *.lof *.toc *.idx *.out *.bbl *.blg *.ilg *.ind *.log
