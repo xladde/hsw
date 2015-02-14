566c944bd192b7edad953144a85e93a30a876b0f
t.j <thomas.jonitz@gmail.com>
2015-02-14 16:27:05 +0100

    update. 	neue Datei:             bpmn2.0/Studentenverwaltung/bewerbungsverfahren.bpmn 	neue Datei:             bpmn2.0/example/Semesterablauf.bpmn 	geändert:               eip/fernst/eip2014.bcf 	geändert:               eip/fernst/eip2014.blg 	geändert:               eip/fernst/eip2014.nav 	geändert:               eip/fernst/eip2014.out 	geändert:               eip/fernst/eip2014.run.xml 	geändert:               eip/fernst/eip2014.snm 	geändert:               eip/fernst/eip2014.tex 	geändert:               eip/fernst/eip2014.toc 	neue Datei:             eip/fernst/eip2014.vrb 	neue Datei:             eip/fernst/img/jonitz.png 	geändert:               eip/fernst/log.txt 	geändert:               eip/fernst/make.sh 	geändert:               tex/hswbeamer.cls

--- eip/fernst/make.sh
+++ eip/fernst/make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pdflatex -halt-on-error -draftmode eip2014.tex  > log.txt
-bibtex eip2014.aux
+#bibtex eip2014.aux
 makeindex eip2014.toc eip2014.lof eip2014.lot
-bibtex eip2014.aux
-pdflatex -halt-on-error -draftmode eip2014.tex >> log.txt
+#bibtex eip2014.aux
+#pdflatex -halt-on-error -draftmode eip2014.tex >> log.txt
 pdflatex --file-line-error -halt-on-error eip2014.tex
 
 #rm *.aux *.lot *.lof *.toc *.idx *.out *.bbl *.blg *.ilg *.ind *.log *.nav *.snm *.txt
